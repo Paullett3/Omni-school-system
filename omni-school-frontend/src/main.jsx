10c719877ea64f2e1ed9b5459068be46023b97f7
Paullett3 <paulletndungu7@gmail.comm>
2026-03-17 10:13:00 +0300

    fix ; Create page in tailwindconfig.js, viteconfig.js and main.jsx

--- omni-school-frontend/src/main.jsx
+++ omni-school-frontend/src/main.jsx
@@ -9,6 +9,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx"; // Junior tip: Make sure the ./ is there!
 import "./index.css"; // THIS LINE LINKS TAILWIND TO YOUR APP
 import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
